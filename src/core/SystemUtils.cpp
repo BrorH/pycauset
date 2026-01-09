f54a06915902f94a9f07a9048b164b2f77f2fa76
brorh <bahjemga@uio.no>
2026-01-09 19:08:06 +0000

    Build: Add support for retrieving RAM information on macOS in SystemUtils

--- src/core/SystemUtils.cpp
+++ src/core/SystemUtils.cpp
@@ -2,6 +2,11 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#elif defined(__APPLE__)
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <mach/mach.h>
+#include <unistd.h>
 #else
 #include <unistd.h>
 #include <sys/sysinfo.h>
@@ -15,6 +20,13 @@ uint64_t SystemUtils::get_available_ram() {
     statex.dwLength = sizeof(statex);
     if (GlobalMemoryStatusEx(&statex)) {
         return statex.ullAvailPhys;
+    }
+    return 0;
+#elif defined(__APPLE__)
+    mach_msg_type_number_t count = HOST_VM_INFO_COUNT;
+    vm_statistics_data_t vm_stat;
+    if (host_statistics(mach_host_self(), HOST_VM_INFO, (host_info_t)&vm_stat, &count) == KERN_SUCCESS) {
+        return (uint64_t)vm_stat.free_count * sysconf(_SC_PAGESIZE);
     }
     return 0;
 #else
@@ -32,6 +44,17 @@ uint64_t SystemUtils::get_total_ram() {
     statex.dwLength = sizeof(statex);
     if (GlobalMemoryStatusEx(&statex)) {
         return statex.ullTotalPhys;
+    }
+    return 0;
+#elif defined(__APPLE__)
+    int mib[2];
+    int64_t physical_memory;
+    size_t length;
+    mib[0] = CTL_HW;
+    mib[1] = HW_MEMSIZE;
+    length = sizeof(int64_t);
+    if (sysctl(mib, 2, &physical_memory, &length, NULL, 0) == 0) {
+        return physical_memory;
     }
     return 0;
 #else
